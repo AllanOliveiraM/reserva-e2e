5edf0521e6ba05d9f22c3efecb6ef96c2c67917b
Allan Oliveira <nallamo2016@gmail.com>
2022-09-02 16:58:18 -0300

    fix: remove unused code

--- cypress/support/core/commands.ts
+++ cypress/support/core/commands.ts
@@ -1,7 +1,3 @@
-type CookieStore = {
-  cookie: Cypress.Cookie | null
-}
-
 Cypress.Commands.add('visualWait', time => {
   if (Cypress.env('useVisualSpeeddown')) {
     // eslint-disable-next-line cypress/no-unnecessary-waiting
